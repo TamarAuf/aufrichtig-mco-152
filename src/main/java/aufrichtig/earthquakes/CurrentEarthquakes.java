925c832793c991d480461da91bdd3d49c3e3d511
yaeldina <yaeldinaa@gmail.com>
2020-05-10 16:13:59 -0700

    added weather classes

---
+++ src/main/java/aufrichtig/earthquakes/CurrentEarthquakes.java
@@ -0,0 +1,26 @@
+package aufrichtig.earthquakes;
+
+public class CurrentEarthquakes {
+
+    Feature features[];
+
+    class Properties {
+        double mag;
+        String place;
+        long time;
+
+        @Override
+        public String toString() {
+            return "Properties{" +
+                    "mag=" + mag +
+                    ", place='" + place + '\'' +
+                    ", time=" + time +
+                    '}';
+        }
+    }
+
+    class Feature {
+        Properties properties;
+    }
+
+}
